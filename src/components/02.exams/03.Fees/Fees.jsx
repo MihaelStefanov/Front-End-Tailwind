5d9a0ec1cb2d96812e66d0a22eb3b3880a7552f2
MihaelStefanov <mihaelstefanov@abv.bg>
2026-05-26 17:18:44 +0300

    Update Fees.jsx

--- src/components/02.exams/03.Fees/Fees.jsx
+++ src/components/02.exams/03.Fees/Fees.jsx
@@ -17,10 +17,10 @@ export default function Fees() {
                 </div>
 
                 <ul className='flex text-gray-700 font-bold gap-[1.2em] '>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Browser</a></li>
-                    <li><a href="">Radio</a></li>
-                    <li><a href="">Podcast</a></li>
+                    <li><a href="">ЗА НАС</a></li>
+                    <li><a href="">ФОРУМ</a></li>
+                    <li><a href="">БЛОГ</a></li>
+                    <li><a href="">КОНТАКТИ</a></li>
                 </ul>
 
             </header>
@@ -28,60 +28,92 @@ export default function Fees() {
             <section className='flex flex-col justify-center items-center bg-[#54546D] h-[380px] relative mb-[14em]'>
                 <h1 className='absolute top-6 text-3xl font-bold text-white mt-[1em] text-center'>Такси</h1>
 
-                <div style={{ backgroundImage: `url(${calculator})` }}
-                    className='w-[420px] h-[250px] left-[0px] top-[4em] bg-cover bg-center self-center absolute z-[5em]'>
-
-                </div>
+                <img
+                    src={calculator}
+                    className='left-[-3em] top-[5em] self-center absolute z-[50]'
+                />
 
                 <div className='flex items-stretch justify-center py-[0em] px-[3em] gap-[2em] z-[10em] absolute bottom-[-10em]'>
 
-                    <div className='flex flex-col overflow-hidden w-[480px] rounded-[0.8em] shadow-xl/20'>
+                    <div className='flex flex-col overflow-hidden w-[480px] rounded-[0.8em] shadow-[0_4px_16px_rgba(0,0,0,0.15)]'>
                         <div className='flex flex-col flex-1 bg-white px-[2em] pt-[1.5em] items-center'>
                             <h1 className='text-[1.8em] mb-[0.2em] font-bold opacity-65'>Design Basics</h1>
                             <p className='text-[1.2em]'>Подготвителен курс</p>
 
-                            <button className='text-white text-center font-bold rounded-[2em] text-[1.2em] bg-[#7d36ce] px-[2.5em] py-[0.5em] my-[2.5em]'>Кандидатствай</button>
+                            <button className='text-white text-center font-bold rounded-[2em] text-[1.3em] bg-[#9A5DC6] px-[2.5em] py-[0.8em] my-[2.5em]
+                            shadow-[0_4px_16px_rgba(0,0,0,0.15)]
+                            '>Кандидатствай</button>
 
                             {/* <ul className='mb-[1.5em] text-[1.1em] leading-[1.8] list-disc text-sm opacity-75 pl-[1.5em]'>
                                 {planInfo.map(data => <li>{data}</li>)}
                             </ul> */}
                         </div>
 
-                        <footer className='bg-[#4f9a39] text-[1.3em] text-white text-center p-[1em] mt-auto'>
-                            <h1>Безплатно</h1>
+                        <footer className='bg-gradient-to-r from-[#179447] via-[#1CB054] to-[#2fd470]
+                         text-[1.3em] text-white text-center p-[1em] mt-auto'>
+                            <h1 className='text-[1.5em]'>Безплатно</h1>
                             <p>* 50 лв. такса за приемен изпит</p>
                         </footer>
                     </div>
 
-                    <div className='flex flex-col  overflow-hidden w-[480px] rounded-[0.8em] shadow-xl/20 '>
+                    <div className='flex flex-col  overflow-hidden w-[480px] rounded-[0.8em] shadow-[0_4px_16px_rgba(0,0,0,0.15)]'>
                         <div className='flex flex-col bg-white px-[1.5em] pt-[1.5em] mb-[1em] items-center text-center'>
                             <h1 className='text-[1.8em] mb-[0.5em] font-bold opacity-65'>Fundamentals Program</h1>
                             <p className='text-[1.2em] mb-[1em]'>Курсистите ще продължат обучението си в 6 фундаментални курса, покриващи основните сфери на дизайна и визуализацията.</p>
                             <p className='text-[1.2em] mb-[1em] '>Възможност за заплащане на таксата на 2 равни вноски.</p>
                         </div>
 
-                        <footer className='bg-[#4f9a39] text-[1.3em] text-white text-center p-[1em] '>
-                            <h1>Безплатно</h1>
-                            <p>* 50 лв. такса за приемен изпит</p>
+                        <footer className='flex bg-gradient-to-r from-[#3d6080] to-[#6fa0c8] text-white p-6
+                        text-[1.3em] text-white text-center px-[0.4em] py-auto
+                        justify-between gap-[0.3em]
+                        '>
+                            <div className='w-[49%]'>
+                                <h1 className='text-[1.3em]'>680 лв.</h1>
+                                <p>присъствено за 6 курса</p>
+                            </div>
+                            <div className='border-r border-white/90 h-[]'></div>
+                            <div className='w-[49%] items-end'>
+                                <h1 className='text-[1.3em]'>620 лв.</h1>
+                                <p>онлайн за 6 курса</p>
+                            </div>
                         </footer>
                     </div>
 
-                    <div className='flex flex-col  overflow-hidden w-[480px] rounded-[0.8em] shadow-xl/20'>
+                    <div className='flex flex-col  overflow-hidden w-[480px] rounded-[0.8em] shadow-[0_4px_16px_rgba(0,0,0,0.15)]'>
                         <div className='flex flex-col bg-white px-[2em] pt-[1.5em] items-center text-center'>
                             <h1 className='text-[1.8em] mb-[0.5em] font-bold opacity-65'>Professional Program</h1>
                             <p className='text-[1.2em]  mb-[1em]'>Курсистите преминават към обучение в професионалните модули, специализирайки се в избраното професионално направление.</p>
                             <p className='text-[1.2em] mb-[1em]'>Обявената такса е преференциална за курсистите от Fundamentals Program.</p>
-
-                            {/* <ul className='mb-[1.5em] text-[1.1em] leading-[1.8] list-disc text-sm opacity-75 pl-[1.5em]'>
-                                {planInfo.map(data => <li>{data}</li>)}
-                            </ul> */}
                         </div>
 
-                        <footer className='bg-[#4f9a39] text-[1.3em] text-white text-center p-[1em] mt-auto'>
-                            <h1>330 лв.</h1>
-                            <p>присъствено за модул</p>
-
+                        <footer className='flex bg-gradient-to-r from-[#4a3a8a] via-[#604EB8] to-[#7e6fd4] p-6
+                        text-[1.3em] text-white text-center px-[0.4em] py-auto
+                        mt-[auto] justify-between gap-[0.3em]
+                        '>
+                            <div className='w-[49%]'>
+                                <h1 className='text-[1.3em]'>330 лв.</h1>
+                                <p>присъствено за модул</p>
+                            </div>
+                            <div className='border-r border-white/90'></div>
+                            <div className='w-[49%] items-end'>
+                                <h1 className='text-[1.3em]'>290 лв.</h1>
+                                <p>онлайн за модул</p>
+                            </div>
                         </footer>
+
+                        {/* <footer className='flex bg-gradient-to-r from-[#4a3a8a] via-[#604EB8] to-[#7e6fd4]
+                        text-[1.3em] text-white text-center px-[0.4em] py-auto'>
+                            <div className='w-[49%]'>
+                                <h1 className='text-[1.3em]'>330 лв.</h1>
+                                <p>присъствено за модул</p>
+                            </div>
+                            <div className='border-r border-white/90 h-[]'></div>
+                            <div className='w-[49%] items-end'>
+                                <h1 className='text-[1.3em]'>290 лв.</h1>
+                                <p>онлайн за модул</p>
+                            </div>
+                        </footer> */}
+
                     </div>
                 </div>
 
@@ -94,21 +126,21 @@ export default function Fees() {
                 <p className='mb-[1em] '>Формите на обучение в SoftUni Digital са две – присъствена и онлайн, така че обучението е подходящо, както за учещи и работещи, така и за курсисти, живеещи във всяка точка на света. Студентите могат да избират дали желаят да се обучават присъствено или онлайн за всеки отделен модул от учебния план.</p>
 
                 <table className=' table-online w-[1260px] overflow-hidden rounded-[0.3em] ' >
-                    <tr> <td>Ad-free music listening</td> <td><img src={checkImg} className="w-4 h-4 inline-block" /></td> <td><img src={crossImg} className="w-4 h-4 inline-block" /></td></tr>
-                    <tr> <td>Ad-free music listening</td> <td><img src={checkImg} className="w-4 h-4 inline-block" /></td> <td><img src={crossImg} className="w-4 h-4 inline-block" /></td></tr>
-                    <tr> <td>Ad-free music listening</td> <td><img src={checkImg} className="w-4 h-4 inline-block" /></td> <td><img src={crossImg} className="w-4 h-4 inline-block" /></td></tr>
-                    <tr> <td>Ad-free music listening</td> <td><img src={checkImg} className="w-4 h-4 inline-block" /></td> <td><img src={crossImg} className="w-4 h-4 inline-block" /></td></tr>
-                    <tr> <td>Ad-free music listening</td> <td><img src={checkImg} className="w-4 h-4 inline-block" /></td> <td><img src={crossImg} className="w-4 h-4 inline-block" /></td></tr>
-                    <tr> <td>Ad-free music listening</td> <td><img src={checkImg} className="w-4 h-4 inline-block" /></td> <td><img src={crossImg} className="w-4 h-4 inline-block" /></td></tr>
+                    <tr> <td>Присъствие в учебните зали на всички занятия от програмата</td> <td><img src={checkImg} className="w-4 h-4 inline-block" /></td> <td><img src={crossImg} className="w-4 h-4 inline-block" /></td></tr>
+                    <tr> <td>Възможност за личен контакт с преподавателя и курсистите на живо</td> <td><img src={checkImg} className="w-4 h-4 inline-block" /></td> <td><img src={crossImg} className="w-4 h-4 inline-block" /></td></tr>
+                    <tr> <td>Онлайн обучение в реално време и възможност за въпроси</td> <td><img src={checkImg} className="w-4 h-4 inline-block" /></td> <td><img src={crossImg} className="w-4 h-4 inline-block" /></td></tr>
+                    <tr> <td>Доживотен достъп до видео записите и учебните ресурси от всични занятия</td> <td><img src={checkImg} className="w-4 h-4 inline-block" /></td> <td><img src={crossImg} className="w-4 h-4 inline-block" /></td></tr>
+                    <tr> <td>Издаване на държавно признати удостоверения от МОН</td> <td><img src={checkImg} className="w-4 h-4 inline-block" /></td> <td><img src={crossImg} className="w-4 h-4 inline-block" /></td></tr>
+                    <tr> <td>Достъп до кариерен център и съдействие за стартиране на работа</td> <td><img src={checkImg} className="w-4 h-4 inline-block" /></td> <td><img src={crossImg} className="w-4 h-4 inline-block" /></td></tr>
                 </table>
             </section>
 
             <section className='flex justify-center items-center gap-[6em]  mb-[6em]'>
                 <div className='w-[800px]'>
-                    <h3 className='mb-[1.2em] font-bold text-[1.2em]'>Many ways to pay and manage your plans easily, quickly and securely with DogoPal</h3>
-                    <p className='mb-[1.2em]' >1. We do not share all of your financial information with sellers. Rest assured that your eligible purchases are protected by DogoPal Buyer Protection. Terms apply.</p>
-                    <p className='mb-[1.2em]'>2. You can easily and securely spend, send money and manage your transactions - all in one place. Download the app to your smartphone or open an account online for free.</p>
-                    <p className='mb-[1.2em]'>3. Pay flexibly with DogoPal. Send and receive money. Support good causes that are close to your heart. Who would have thought that an app could be so versatile?</p>
+                    <h3 className='mb-[1.2em] font-bold text-[1.2em]'>Финансиране от Пощенска банка</h3>
+                    <p className='mb-[1.2em]' >Всички курсисти могат да се възползват от преференциалните условия за потребителски кредит и овърдрафт, за финансиране на своето обучение в СофтУни, като заявят лесно и удобно избрания продукт:</p>
+                    <p className='mb-[1.2em]'>При последващ разговор със служител на банката, следва да се уточни, че искането е за кредит за финансиране на обучение в SoftUni и да се предостави студентски номер.
+                        www.postbank.bg - при попълване на апликацията, в поле „промоционален код“ трябва да се въведe SoftUni/ СофтУни и при последващ разговор със служител на банката, да се предостави студентски номер.</p>
                 </div>
 
                 <div
